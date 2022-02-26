b912b2087348e909394210710a201ec485cde0d5
Rubén <gonzru01atwork@gmail.com>
2022-02-25 23:53:45 -0400

    login and sign up functional

--- src/php/procesar.php
+++ src/php/procesar.php
@@ -7,16 +7,54 @@ include('config.php');
 $lastPage = $_SERVER['HTTP_REFERER'];
 
 if($lastPage == "http://localhost/inventory-system/login.html") {
-    echo "vengo de login.html";
+
     $lCorreo = $_POST['lmail'];
     $lPassword = $_POST['lpassword'];
-    echo $lCorreo . " " . $lPassword;
+    $query = "SELECT correo FROM usuario WHERE contraseña='$lPassword' AND correo='$lCorreo'";
+
+    if(!$conn){
+        die("Conexión fallida - " . mysqli_connect_error());
+    }
+
+    $result = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($result)  == 1){
+        echo "Inicio de sesion exitoso";
+    } else {
+        echo "error de datos de inicio de sesion";
+    }
+
+
+
 } elseif($lastPage == "http://localhost/inventory-system/sign-up.html") {
-    echo "vengo de sign-up.html";
+
     $nombre = $_POST['name'];
     $correo = $_POST['mail'];
     $password = $_POST['password'];
+
+    if(!$conn) {
+        die("Conexión fallida - " . mysqli_connect_error());
+    }
+
+    if(isset($nombre) && isset($correo) && isset($password)){
+        
+        $query0 = "INSERT INTO usuario (correo, contraseña, nombre) VALUES ('$correo', '$password', '$nombre')";
+        $query1 = "SELECT correo FROM usuario WHERE correo='$correo'";
+        $result = mysqli_query($conn, $query1);
+
+        if(mysqli_num_rows($result) > 0){
+            echo 'Ya existe una cuenta asociada a este correo';
+        } else {
+            if(mysqli_query($conn, $query0)) {
+                echo "Ingresado correctamente a la base de datos";
+            } else {
+                echo "Error " . mysqli_errno($conn);
+            }
+        }
+        
+    }
 }
 
+mysqli_close($conn);
 
 ?>
